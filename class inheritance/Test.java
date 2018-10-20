ec99904f2ab34f1441abf7b94c4c786cf8a11328
Alex <dyywinner@126.com>
2018-10-20 22:01:11 +0800

    Create Test.java

---
+++ class inheritance/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        JavaEE ee = new JavaEE("zhang3","developer001");
+        ee.work();
+        //
+        Net net = new Net("li4","maintan005");
+        net.work();
+    }
+}
